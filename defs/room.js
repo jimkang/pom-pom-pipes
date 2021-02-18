3214f045a48db9e2dbb2ac8abbb17b2840f56e74
Jim Kang <jimkang@gmail.com>
2021-02-17 20:21:40 -0500

    Make sure exit positions are not duplicated in a room.

--- defs/room.js
+++ defs/room.js
@@ -1,10 +1,11 @@
-//var probable = require('probable');
+var { rollDie, shuffle, createTableFromSizes } = require('probable');
 var { Tablenest, r } = require('tablenest');
 var tablenest = Tablenest();
+var cloneDeep = require('lodash.clonedeep');
 //var range = require('d3-array').range;
 
 var roomDef = {
-  root: [[1, r({ roomType: r`roomType` })]],
+  root: [[1, r({ roomType: r`roomType`, exits: [] })]],
   roomType: [
     [2, { shape: 'room', size: 'small', height: 'short' }],
     [1, { shape: 'room', size: 'small', height: 'tall' }],
@@ -15,9 +16,39 @@ var roomDef = {
   ]
 };
 
+var exitPositionDeck = ['north', 'east', 'south', 'west', 'floor'];
+
+var wallExitKindTable = createTableFromSizes([
+  [1, 'direct connection'],
+  [2, 'pipe, vertical'],
+  [1, 'pipe, vertical, with rungs'],
+  [2, 'pipe, sloped']
+]);
+var floorExitKindTable = createTableFromSizes([
+  [1, 'direct connection'],
+  [3, 'pipe, horizontal'],
+  [2, 'pipe, sloped']
+]);
+
+var rollTheRoom = tablenest(roomDef);
+
 function rollRoom() {
-  var roll = tablenest(roomDef);
-  return roll();
+  var room = cloneDeep(rollTheRoom());
+  const exitCount = rollDie(4);
+  var positions = shuffle(exitPositionDeck);
+  console.log('positions', positions);
+  for (var i = 0; i < exitCount; ++i) {
+    const position = positions[i];
+    let kind;
+    if (position === 'floor') {
+      kind = floorExitKindTable.roll();
+    } else {
+      kind = wallExitKindTable.roll();
+    }
+    room.exits.push({ position, kind });
+  }
+
+  return room;
 }
 
 module.exports = rollRoom;
